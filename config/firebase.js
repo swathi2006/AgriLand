2161623a998dfa56708f865933858e32654f620d
swathi2006 <149090342+swathi2006@users.noreply.github.com>
2025-04-05 20:50:54 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,13 @@
 const admin = require("firebase-admin");
 const path = require("path");
 require("dotenv").config();
-
-const serviceAccount = require(path.resolve(process.env.FIREBASE_KEY_PATH));
+const firebaseKey = JSON.parse(process.env.FIREBASE_KEY_PATH);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseKey),
 });
 
 const db = admin.firestore();
 
 module.exports = { admin, db };
+
